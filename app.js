9d5b5eb5723e968bcd8e40d09fb21cb198d698d0
Gooey12 <125751318+CaptGooey12@users.noreply.github.com>
2024-05-23 13:06:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ let QRPicture = document.getElementById("QRPicture");
 
 function genQR() {
     QRPicture.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + Text.value;
+
+    Text.value = '';
 }
